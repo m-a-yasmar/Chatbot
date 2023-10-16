7f763732eac1cb4a627774564c3ac93f05b75c16
Michael Ramsay <96756649+m-a-yasmar@users.noreply.github.com>
2023-10-16 01:34:36 +0100

    Update chatbot_backend.py

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -423,7 +423,7 @@ def ask():
             "Authorization": f"Bearer {os.environ.get('OPENAI_API_KEY')}",
             "Content-Type": "application/json"
         }
-        prompt = f"Answer the following questions as if you're an expert on Jamaica. Do not identify yourself as an AI. If you can answer just State that you cant answer in the most human way. In your answersing where possible consider your previous answers  : {query}"
+        prompt = f"Answer the following questions as if you're an expert on Jamaica. Do not identify yourself as an AI : {query}"
        
         payload = {
             "model": "gpt-4",  
