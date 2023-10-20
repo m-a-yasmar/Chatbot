47aff6f6f678cb8cb238579a3521757d560b00f4
Michael Ramsay <96756649+m-a-yasmar@users.noreply.github.com>
2023-10-21 00:18:52 +0100

    Update chatbot_backend.py
    temp removed max tokens

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -466,9 +466,9 @@ def ask():
             "model": "gpt-4",
             "messages": conversation_with_prompt,
             "frequency_penalty": 1.5,  
-            "presence_penalty": -1,
-            "max_tokens": 80
+            "presence_penalty": -1
         }
+        #             "max_tokens": 80
         # frequency -2 to 2. higher increase repetition of answer  presence -2 to 2. higher likely to switch topic
         #response = requests.post(api_endpoint, headers=headers, json=payload)
         response = requests.post(api_endpoint, headers=headers, json=payload, timeout=60)  # 15-second timeout
