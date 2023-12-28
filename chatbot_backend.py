beee37687a79fd1d9d007c79873edfd03aa9236a
Michael Ramsay <96756649+m-a-yasmar@users.noreply.github.com>
2023-12-28 14:29:54 +0000

    Update chatbot_backend.py

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -93,15 +93,13 @@ def serve_image(filename):
     
 @chatbot.before_request
 def setup_conversation():
-    if 'conversation' not in session:
+    if 'session_id' not in session:
+	session['session_id'] = str(uuid4())
+        print("New session being initialized with ID:", session['session_id'])
         session['conversation'] = [{"role": "system", "content": "You are an AI agent representing TalkAI Global, specializing in AI automation. Your primary role is to engage in a two-way conversation with users, focusing on understanding their needs and responding with insightful information about our AI services. Be concise yet informative, responding in a way that is not overwhelming. Ask relevant questions to gather user requirements and listen attentively to their queries. Provide brief, clear answers and encourage further questions or direct contact for detailed discussions, especially regarding pricing and service customization. Your aim is to create a connection by being an attentive listener and a knowledgeable guide in the world of AI solutions."} ]
 	
-        session['session_id'] = str(uuid4())  # Generate a unique session ID
-        print("New session being initialized with ID:", session['session_id'])
-    else:
-	    print("Existing session found with ID:", session.get('session_id'))
-    print("Initial session:", session.get('conversation'))
-    
+    print("Current session ID:", session['session_id'])
+
 limiter = Limiter(
     app=chatbot,
     key_func=get_remote_address
@@ -130,18 +128,17 @@ def services():
 
 
 
-
 @chatbot.route('/ask', methods=['POST'])
 def ask():
     system_message = {}
-    threshold = 0.7
+    threshold = 0.9
     query = request.json.get('query')
     print("User query:", query)
 
-    max_tokens = 20  # Set your desired limit
+    max_tokens = 30  # Set your desired limit
     tokens = query.split()
     if len(tokens) > max_tokens:
-        answer = "Your query is too long. Please limit it to 20 words or less."
+        answer = "Your query is too long. Please limit it to 30 words or less."
         return jsonify({"answer": answer})
 
     session['conversation'].append({"role": "user", "content": query})
